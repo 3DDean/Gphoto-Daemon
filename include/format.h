dc015684e60546fbe15f6020c97370b62cb89376
Darrel <3DDeans@gmail.com>
2022-11-04 10:48:33 -0500

    Added ref_counter

--- include/format.h
+++ include/format.h
@@ -6,7 +6,6 @@ struct formatting_error
 {};
 
 // https://www.partow.net/programming/hashfunctions/
-
 constexpr uint32_t ELFHash(const char *str, unsigned int length)
 {
 	unsigned int hash = 0;
@@ -28,12 +27,14 @@ constexpr uint32_t ELFHash(const char *str, unsigned int length)
 	return hash;
 }
 
+// Not using the hash functions because I don't think my approach is really working
 template <typename T>
 struct hash
 {
 	using hash_t = uint32_t;
 
 	constexpr hash(T &obj)
+		// I think this is just hashing the
 		: hash_code(ELFHash((char *)&obj, sizeof(T)))
 	{}
 
@@ -101,7 +102,10 @@ constexpr format_type get_type_enum(std::string_view _str)
 struct format_var
 {
 	using hash_t = size_t;
-	constexpr format_var(){}
+
+	constexpr format_var(const format_var&) = default;
+
+	constexpr format_var() {}
 
 	constexpr format_var(std::string_view str)
 	{
@@ -125,15 +129,29 @@ struct format_var
 				label = var;
 			}
 		}
-		else{
+		else
+		{
 			throw formatting_error{};
 		}
 	}
-
-	constexpr format_var(std::string_view _type, std::string_view _label) : type(get_type_enum(_type)), label(_label){}
+	bool operator==(const format_var& rhs) const 
+	{
+		return type == rhs.type && label == rhs.label;
+	}
+	constexpr format_var(std::string_view _type, std::string_view _label)
+		: type(get_type_enum(_type)), label(_label) {}
 
 	format_type type;
 	std::string_view label;
+};
+
+template<class Compare = std::less<>>
+struct format_var_label_compare
+{
+	constexpr inline bool operator()(const format_var& lhs, const format_var& rhs)
+	{
+		return Compare{}(lhs.label, rhs.label);
+	}
 };
 
 template <size_t N>
@@ -143,7 +161,6 @@ struct formatted_string
 	std::array<std::string_view, N + 1> constants;
 	std::array<std::string_view, N> vars;
 };
-
 
 template <size_t N>
 struct variable_str_constructor_helper
@@ -238,14 +255,84 @@ struct variable_str_constructor_helper
 	}
 };
 
+template <typename Key, typename T, std::size_t N, class Compare>
+struct ref_counter
+{
+	using value_type = std::pair<Key, T>;
+
+	using Array = std::array<value_type, N>;
+	using iterator = typename Array::iterator;
+	Array data;
+	size_t count = 0;
+	
+	iterator first = data.begin();
+	iterator last = data.begin();
+
+	struct internal_compare
+	{
+		constexpr inline bool operator()(const value_type& lhs, const Key& rhs)
+		{
+			return Compare{}(lhs.first, rhs);
+		}
+	};	
+	constexpr size_t size()
+	{
+		return last - data.begin();
+	}
+
+	constexpr auto begin()
+	{
+		return data.begin();
+	}
+
+	constexpr auto end()
+	{
+		return last;
+	}
+
+	constexpr void insert(Key &_str)
+	{
+		auto result = std::lower_bound(begin(), last, _str, internal_compare{});
+		
+		if (result == last)
+		{
+			*last = value_type{_str, 1};
+			last++;
+		}
+		else
+		{
+			//TODO move found behavior into template parameters
+			if((*result).first == _str)
+			{
+				(*result).second++;
+				return;
+			}
+			else
+			{
+				std::move_backward(result, last, last + 1);
+				*result = value_type(_str, 1);
+				last++;
+			}
+		}
+	}
+};
+
 template <size_t N>
 constexpr auto get_vars(const std::string_view _str)
 {
 	variable_str_constructor_helper<N> formatted_string(_str);
-	auto [var_data, str_data]= formatted_string.get_var_array();
+	auto [var_data, str_data] = formatted_string.get_var_array();
 
-	//TODO count number of bytes required for new constant array
-	//TODO remove duplicate variables and replace them with a position arg
+	using var_usage = std::pair<std::string_view, size_t>;
+
+	ref_counter<format_var, size_t, N, format_var_label_compare<>> refCounter;
+	for (auto var : var_data)
+	{
+		refCounter.insert(var);
+	}
+
+	// TODO count number of bytes required for new constant array
+	// TODO remove duplicate variables and replace them with a position arg
 
 	return str_data;
 }
